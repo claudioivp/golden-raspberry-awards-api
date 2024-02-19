02662ccf3121ba7a38b765ced64df86fa9b539b8
Cláudio José Antunes Mazur <claudiomazur@maringa.pr.gov.br>
2024-02-19 02:24:46 -0300

    Introduce StudioController for API endpoints, StudioMapper for request/response conversion, and update InsertStudioAdapter for data insertion.

---
+++ src/main/java/com/texoit/goldenraspberryawardsapi/adapters/in/controller/mapper/StudioMapper.java
@@ -0,0 +1,15 @@
+package com.texoit.goldenraspberryawardsapi.adapters.in.controller.mapper;
+
+import com.texoit.goldenraspberryawardsapi.adapters.in.controller.request.StudioRequest;
+import com.texoit.goldenraspberryawardsapi.adapters.in.controller.response.StudioResponse;
+import com.texoit.goldenraspberryawardsapi.adapters.out.repository.entity.StudioEntity;
+import com.texoit.goldenraspberryawardsapi.application.core.domain.Studio;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface StudioMapper {
+
+    Studio toStudio(StudioRequest studioRequest);
+    StudioResponse toStudioResponse(Studio studio);
+
+}
