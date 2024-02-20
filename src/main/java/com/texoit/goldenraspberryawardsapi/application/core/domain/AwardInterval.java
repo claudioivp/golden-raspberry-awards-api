b62b4d10f0572909777115b53d6c438e181103a4
Cláudio José Antunes Mazur <claudiomazur@maringa.pr.gov.br>
2024-02-20 14:20:34 -0300

    Implementing FindMinAwardInterval Functionality

---
+++ src/main/java/com/texoit/goldenraspberryawardsapi/application/core/domain/AwardInterval.java
@@ -0,0 +1,50 @@
+package com.texoit.goldenraspberryawardsapi.application.core.domain;
+
+import com.texoit.goldenraspberryawardsapi.adapters.out.repository.response.ProducerIntervalsProjection;
+
+public class AwardInterval {
+
+    private String producer;
+    private Integer yearInterval;
+    private Integer previousWin;
+    private Integer followingWin;
+
+    public AwardInterval(String producer, Integer yearInterval, Integer previousWin, Integer followingWin) {
+        this.producer = producer;
+        this.yearInterval = yearInterval;
+        this.previousWin = previousWin;
+        this.followingWin = followingWin;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public Integer getYearInterval() {
+        return yearInterval;
+    }
+
+    public void setYearInterval(Integer yearInterval) {
+        this.yearInterval = yearInterval;
+    }
+
+    public Integer getPreviousWin() {
+        return previousWin;
+    }
+
+    public void setPreviousWin(Integer previousWin) {
+        this.previousWin = previousWin;
+    }
+
+    public Integer getFollowingWin() {
+        return followingWin;
+    }
+
+    public void setFollowingWin(Integer followingWin) {
+        this.followingWin = followingWin;
+    }
+}
