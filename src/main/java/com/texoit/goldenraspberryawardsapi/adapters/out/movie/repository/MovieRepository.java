939236dc00879dee186341a11394cc7db80393d3
Cláudio José Antunes Mazur <claudiomazur@maringa.pr.gov.br>
2024-03-15 22:27:08 -0300

    Adding a search by movie title

--- src/main/java/com/texoit/goldenraspberryawardsapi/adapters/out/movie/repository/MovieRepository.java
+++ src/main/java/com/texoit/goldenraspberryawardsapi/adapters/out/movie/repository/MovieRepository.java
@@ -3,7 +3,11 @@ package com.texoit.goldenraspberryawardsapi.adapters.out.movie.repository;
 import com.texoit.goldenraspberryawardsapi.adapters.out.movie.repository.entity.MovieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MovieRepository extends JpaRepository<MovieEntity, UUID> {
+
+    Optional<MovieEntity> findByTitle(String title);
+
 }
