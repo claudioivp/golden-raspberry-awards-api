9cb6ebd0e4294cd71f6de17acee09216ef8cc0af
Cláudio José Antunes Mazur <claudiomazur@maringa.pr.gov.br>
2024-03-19 23:45:00 -0300

    Implement Refreshable - Movie Repo/Adapter/Mapper
    This commit introduces a refreshable repository for the Movie entity, enhancing the existing functionality. Additionally, it includes the implementation of an adapter that utilizes this repository to insert data efficiently. These updates ensure that the Studio repository stays synchronized and up-to-date, improving data integrity and application reliability.

--- src/main/java/com/texoit/goldenraspberryawardsapi/adapters/out/movie/repository/mapper/MovieProducerEntityMapper.java
+++ src/main/java/com/texoit/goldenraspberryawardsapi/adapters/out/movie/repository/mapper/MovieProducerEntityMapper.java
@@ -1,28 +1,16 @@
 package com.texoit.goldenraspberryawardsapi.adapters.out.movie.repository.mapper;
 
-import com.texoit.goldenraspberryawardsapi.adapters.out.producer.repository.ProducerRepository;
 import com.texoit.goldenraspberryawardsapi.adapters.out.producer.repository.entity.ProducerEntity;
 import com.texoit.goldenraspberryawardsapi.application.core.domain.producer.Producer;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public abstract class MovieProducerEntityMapper {
-
-    @Autowired
-    private ProducerRepository producerRepository;
+public interface MovieProducerEntityMapper {
 
     @Mapping(target = "movies", ignore = true)
-    public abstract ProducerEntity toProducerEntity(Producer producer);
+    ProducerEntity toProducerEntity(Producer producer);
     @Mapping(target = "movies", ignore = true)
-    public abstract Producer toProducer(ProducerEntity producerEntity);
-
-    @AfterMapping
-    public ProducerEntity afterMapping(Producer producer, @MappingTarget ProducerEntity producerEntity) {
-        return producerRepository.findById(producer.getId()).orElseThrow();
-    }
+    Producer toProducer(ProducerEntity producerEntity);
 
 }
