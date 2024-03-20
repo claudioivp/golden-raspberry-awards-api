1a75df9f8166f1c783c1e5090593913ee39bf985
Cláudio José Antunes Mazur <claudiomazur@maringa.pr.gov.br>
2024-03-19 23:46:36 -0300

    Applying RefreshableJpaRepositoryImpl in the app init
    This commit introduces the annotation @EnableJpaRepositories with the parameter repositoryBaseClass set to RefreshableJpaRepositoryImpl.class. By configuring this, the application is now empowered to utilize the RefreshableJpaRepository implementation as the base class for JPA repositories. This change enhances the refreshability and reliability of JPA repositories, contributing to a more robust data management system.

--- src/main/java/com/texoit/goldenraspberryawardsapi/GoldenRaspberryAwardsApiApplication.java
+++ src/main/java/com/texoit/goldenraspberryawardsapi/GoldenRaspberryAwardsApiApplication.java
@@ -1,11 +1,14 @@
 package com.texoit.goldenraspberryawardsapi;
 
+import com.texoit.goldenraspberryawardsapi.adapters.out.base.repository.RefreshableJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
+@EnableJpaRepositories(repositoryBaseClass = RefreshableJpaRepositoryImpl.class)
 public class GoldenRaspberryAwardsApiApplication {
 
 	public static void main(String[] args) {
